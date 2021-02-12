12d2811565168e4f56187054cbd07eb3cb9ff6c4
ailenamodio <ailenamodio@gmail.com>
2021-02-11 22:49:16 -0300

    *** commit inicial ***

---
+++ src/defaulPackage/AppGestionPedidos.java
@@ -0,0 +1,12 @@
+package defaulPackage;
+
+import entidades.Producto;
+
+public class AppGestionPedidos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Producto prod =new Producto("","");
+	}
+
+}
